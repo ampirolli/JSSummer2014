1179164948ddc74fb1c3b40863ffa3feafef5ed4
001329606 <001329606@N205C3.labs.neit.edu>
2014-07-29 19:08:43 -0400

    Week1 Demo

--- public_html/Week2/demo/javascript2.js
+++ public_html/Week2/demo/javascript2.js
@@ -1,14 +1,25 @@
-
-
 var email = document.getElementById('email');
-
-console.log(email.value);
-
-
+    var err_email = document.getElementById('err_email');
+    var hasErrors = false;
     
-//method
-function testing(){
-    console.log('hello');
-}
+    function submit() {
+        
+        if ( !email.value.length ) {
+            console.log('email is empty');
+            //.classList
+            //innerHTML
+        } else {
+            console.log('email is not empty');
+        }
+        
+        
+    }
+    
+    
+    
+    // method
+    function testing( str ){
+        console.log(str);
+   }
 
-testing();
+//testing('helloooooooooooo');
