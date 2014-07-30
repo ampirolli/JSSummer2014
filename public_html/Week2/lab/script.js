201df3de4c7da37a3839180fcd9eb46a5ba90c31
001329606 <001329606@N205C3.labs.neit.edu>
2014-07-29 20:13:58 -0400

    Week1 Demo

--- public_html/Week2/lab/script.js
+++ public_html/Week2/lab/script.js
@@ -10,6 +10,10 @@
     
     var fullname = document.getElementById("name");
     var fullnameErr = document.getElementById("err_name");
+    var email = document.getElementById("email");
+    var emailErr = document.getElementById("err_email");
+  //  var comments = document.getElementById("comment");
+  //  var err_comments = document.getElementById("err_comment");
     
     var hasErrors = false;
     
@@ -24,7 +28,27 @@
         fullname.classList.add('bad');       
         fullnameErr.innerHTML = "<p>Full Name is not valid.</p>";       
     }
-    
-    
+     if(email.value.length){
+         hasErrors = true;
+         email.classList.remove('bad');
+         email.classList.add('good');
+         emailErr.innerHTML = '';
+     }else{
+         email.classList.remove('good');
+         email.classList.add('bad');
+         emailErr.innerHTML="<p>Email is not valid.</p>";
+     }
+     
+     if(comments.value.length < 1 || comments.value.length > 250){
+         hasErrors = true;
+         comments.classList.remove('good');
+         comments.classList.add('bad');
+         err_comments.innerHTML="<p>Comments must be between 1 and 250 characters</p>";
+     }else{
+         
+         comments.classList.remove('bad');
+         comments.classList.add('good');
+         err_comments.innerHTML="";
+     }
     
 }
